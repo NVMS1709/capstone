81ccfcfd841fa269db669ec000f341fcf07e1b2c
Mike Romani <mr4227@gmail.com>
2017-11-16 17:24:54 -0500

    updates lost on question-page re-inserted for commit

--- client/components/question-page/index.js
+++ client/components/question-page/index.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 import AceEditor from './aceEditor'
+import Outcome from './outcome'
+import InstructionMode from './instruction'
+import Question from './question'
 
-export default class QuestionPage extends Component {
+export default class questionPage extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      componentToggle: true
+    }
+    this.toggle = this.toggle.bind(this)
   }
+
+  toggle() {
+    this.setState({ componentToggle: !this.state.componentToggle })
+  }
+
   render() {
-    return <AceEditor />
+    return (
+      <div>
+        <Question />
+        <AceEditor />
+        {this.state.componentToggle ? <Outcome /> : <InstructionMode />}
+        {this.state.componentToggle ? (
+          <button onClick={this.toggle}>Instructional Mode</button>
+        ) : (
+          <button onClick={this.toggle}>Solution Mode</button>
+        )}
+      </div>
+    )
   }
 }
