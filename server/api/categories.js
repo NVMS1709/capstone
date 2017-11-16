84d1d380ceab5a8784e5dbf536d957f0c1d11746
NicholasGalarza <nicogala93@gmail.com>
2017-11-16 16:45:41 -0500

    Add structure for categories on home-page

--- server/api/categories.js
+++ server/api/categories.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
-const { Category } = require('../db/models')
+const { Category, Question } = require('../db/models')
+
 module.exports = router
 
 router.get('/', (req, res, next) => {
-    Category.findAll()
+    Category.findAll({include: [Question]})
         .then(category => res.json(category))
         .catch(next)
 })
