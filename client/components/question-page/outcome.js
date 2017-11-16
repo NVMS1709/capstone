dd5891e76588c92edb9ae108f72b7319b258ba9d
Mike Romani <mr4227@gmail.com>
2017-11-15 19:55:28 -0500

    feat(ace-editor) create question-page/route and add ace editor into question-page

--- client/components/question-page/outcome.js
+++ client/components/question-page/outcome.js
@@ -1,41 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import 'brace/mode/java'
-import 'brace/theme/github'
-
 class Outcome extends Component {
   constructor(props) {
     super(props)
-    this.onChange = this.onChange.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
-    this.userSubmissoin = ''
-  }
-
-  onChange(newValue) {
-    console.log('change', newValue)
-    this.userSubmission = newValue
-  }
-
-  onSubmit(event) {
-    event.preventDefault()
-    this.props.getInput(this.userSubmission)
   }
 
   render() {
-    console.log(this.props)
-    return (
-      <div>
-        <AceEditor
-          mode="java"
-          theme="github"
-          onChange={this.onChange}
-          name="user-input"
-          editorProps={{ $blockScrolling: true }}
-        />
-        <button onClick={this.onSubmit}>Submit</button>
-      </div>
-    )
+    return <div />
   }
 }
 
