fd5f67a6ce25134e04988d3b666920e8e37a0463
NicholasGalarza <nicogala93@gmail.com>
2017-11-17 20:05:36 -0500

    Add unit test for category, test data for all catagories and dropzone for dummy data, fixed some pathing with front end tests. BUG: db.model() is not functional and I don't know why

---
+++ server/api/tests/categories.spec.js
@@ -0,0 +1,34 @@
+const Promise = require('bluebird')
+const { expect } = require('chai')
+const request = require('supertest')
+
+const db = require('../../db/models/index')
+const app = require('../../index')
+const Category = db.model('category') // Trouble spot for test
+
+describe('[CATEGORIES ROUTE]', () => {
+  beforeEach(() => {
+    return db.sync({ force: true })
+  })
+
+  describe('[API] Categories Route Tests', () => {
+
+    beforeEach(() => {
+      return Category.create({
+        name: 'Arrays',
+        description: 'An array is a collection of items stored at continuous memory locations. Each item (element) is accessible by their index.'
+      })
+    })
+
+    it('GET request to /api/categories/', () => {
+      return request(app)
+        .get('/api/categories')
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('array')
+          expect(res.body[0].name).to.be.equal('Arrays')
+          expect(res.body[0].description).to.be.equal('An array is a collection of items stored at continuous memory locations. Each item (element) is accessible by their index.')
+        })
+    })
+  })
+})
