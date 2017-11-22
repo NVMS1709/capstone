b72d2ab9b3cfb5848cebda2d2ac6bc2430501d85
NicholasGalarza <nicogala93@gmail.com>
2017-11-22 18:46:07 -0500

    vertical alignment

--- client/components/user-account-page/categoryMap.js
+++ client/components/user-account-page/categoryMap.js
@@ -2,36 +2,34 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class CategoryMap extends Component {
-	constructor(props) {
-		super(props)
-	}
+  constructor(props) {
+    super(props)
+  }
 
-	render() {
-		const categories = this.props.categories
-		console.log(categories)
-		return (
-			<div className="categories-section">
-				<div className="categories">
-					{categories.map(category => {
-						return (
-							<div key={category.id} className="category-child">
-								<div className="half-n-half">
-									<h6 className="half-child">{category.name}</h6>
-									<p className="half-child">{category.questions.length}</p>
-								</div>
-							</div>
-						)
-					})}
-				</div>
-			</div>
-		)
-	}
+  render() {
+    const categories = this.props.categories
+    console.log(categories)
+    return (
+      <div className="categories-section">
+        <div className="categories">
+          {categories.map(category => {
+            return (
+              <div key={category.id} className="category-child">
+                <div className="half-child">{category.name}</div>
+                <div className="half-child">{category.questions.length}</div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
 }
 
 const mapState = state => {
-	return {
-		categories: state.categories
-	}
+  return {
+    categories: state.categories
+  }
 }
 
 export default connect(mapState)(CategoryMap)
