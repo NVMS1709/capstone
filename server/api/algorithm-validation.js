4a9343da030135db262f7c04a932c696c48639a3
Mike Romani <mr4227@gmail.com>
2017-11-27 12:20:24 -0500

    fix(forum)

--- server/api/algorithm-validation.js
+++ server/api/algorithm-validation.js
@@ -3,136 +3,145 @@ const fs = require('fs')
 const path = require('path')
 const { exec } = require('child_process')
 const {
-    getTestCaseOutcomes,
-    wrapTestfile,
-    wrapAlgorithmInput
+  getTestCaseOutcomes,
+  wrapTestfile,
+  wrapAlgorithmInput
 } = require('./algorithm-execution-util')
 const tmp = require('tmp')
 
 module.exports = router
 router.post('/javascript', (req, res, next) => {
+  req.body.submission.algorithmInput = wrapAlgorithmInput(
+    req.body.submission.algorithmInput,
+    req.body.submission.functionName
+  )
 
-    req.body.submission.algorithmInput = wrapAlgorithmInput(
-        req.body.submission.algorithmInput,
-        req.body.submission.functionName
+  req.body.submission.testFile = wrapTestfile(
+    req.body.submission.testFile,
+    req.body.submission.functionName
+  )
+
+  const createAlgorithmTestTempDirectory = () =>
+    new Promise((resolve, reject) => {
+      tmp.dir(
+        {
+          unsafeCleanup: true,
+          prefix: 'test_',
+          dir: path.join(__dirname, '..', 'algorithm_input_test')
+        },
+        (err, tmpPath, cleanupCB) => {
+          if (err) {
+            reject(err)
+            return next(err)
+          }
+          resolve([tmpPath, cleanupCB])
+        }
+      )
+    })
+
+  const createAlgorithmInputFile = (algorithmInput, tempDirectory) =>
+    new Promise((resolve, reject) => {
+      fs.writeFile(
+        path.join(tempDirectory, 'algorithm-input.js'),
+        algorithmInput,
+        err => {
+          if (err) {
+            reject(err)
+            return next(err)
+          }
+          resolve()
+        }
+      )
+    })
+
+  const createAlgorithmTestFile = (algorithmTest, tempDirectory) =>
+    new Promise((resolve, reject) => {
+      fs.writeFile(
+        path.join(tempDirectory, 'algorithm-test.js'),
+        algorithmTest,
+        err => {
+          if (err) {
+            reject(err)
+            return next(err)
+          }
+          resolve()
+        }
+      )
+    })
+
+  ;(async function(algorithmInput, algorithmTest) {
+    const [
+      algorithmTestTempDirectory,
+      cleanupCB
+    ] = await createAlgorithmTestTempDirectory()
+
+    const algorithmTestFile = createAlgorithmTestFile(
+      algorithmTest,
+      algorithmTestTempDirectory
     )
 
-    req.body.submission.testFile = wrapTestfile(
-        req.body.submission.testFile,
-        req.body.submission.functionName
+    const algorithmInputFile = createAlgorithmInputFile(
+      algorithmInput,
+      algorithmTestTempDirectory
     )
 
-    const createAlgorithmTestTempDirectory = () => (
-        new Promise((resolve, reject) => {
-            tmp.dir({ unsafeCleanup: true, prefix: 'test_', dir: path.join(__dirname, '..', 'algorithm_input_test') }, (err, tmpPath, cleanupCB) => {
-                if (err) {
-                    reject(err)
-                    return next(err)
-                }
-                resolve([tmpPath, cleanupCB])
-            })
-        })
-    );
+    return [
+      algorithmTestTempDirectory,
+      cleanupCB,
+      await algorithmInputFile,
+      await algorithmTestFile
+    ]
+  })(req.body.submission.algorithmInput, req.body.submission.testFile)
+    .then(([algorithmTestTempDirectory, cleanupCB]) => {
+      exec(
+        `npm run test-javascript-algorithm-input ./server/algorithm_input_test${algorithmTestTempDirectory.slice(
+          algorithmTestTempDirectory.lastIndexOf('/')
+        )}/algorithm-test.js`,
+        { timeout: 5000 },
+        (err, stdout, stderr) => {
+          try {
+            const { testCasesStr, revisedStdoutStr } = getTestCaseOutcomes(
+              stdout
+            )
 
-    const createAlgorithmInputFile = (algorithmInput, tempDirectory) => (
-        new Promise((resolve, reject) => {
-            fs.writeFile(path.join(tempDirectory, 'algorithm-input.js'), algorithmInput, (err) => {
-                if (err) {
-                    reject(err)
-                    return next(err)
-                }
-                resolve()
-            })
-        })
-    );
+            const testCasesArr = JSON.parse(testCasesStr.trim())
 
-    const createAlgorithmTestFile = (algorithmTest, tempDirectory) => (
-        new Promise((resolve, reject) => {
-            fs.writeFile(path.join(tempDirectory, 'algorithm-test.js'), algorithmTest, (err) => {
-                if (err) {
-                    reject(err)
-                    return next(err)
-                }
-                resolve()
-            })
-        })
-    );
+            if (err) {
+              cleanupCB()
+              console.error('EXECUTION ERROR______________________', err)
+            }
 
-    (async function (algorithmInput, algorithmTest) {
+            cleanupCB()
 
-        const [
-            algorithmTestTempDirectory,
-            cleanupCB
-        ] = await createAlgorithmTestTempDirectory()
+            let results
 
-        const algorithmTestFile = createAlgorithmTestFile(algorithmTest, algorithmTestTempDirectory)
+            if (
+              testCasesArr &&
+              !testCasesArr.find(testCase => testCase.outcome === 'failed')
+            ) {
+              results = {
+                testCasesArr,
+                rawOutput: '\n' + stderr + '\n' + revisedStdoutStr,
+                allPassed: true
+              }
+            } else {
+              results = {
+                testCasesArr,
+                rawOutput: '\n' + stderr + '\n' + revisedStdoutStr,
+                allPassed: false
+              }
+            }
 
-        const algorithmInputFile = createAlgorithmInputFile(algorithmInput, algorithmTestTempDirectory)
-
-        return [
-            algorithmTestTempDirectory,
-            cleanupCB,
-            await algorithmInputFile,
-            await algorithmTestFile
-        ]
-
-    })(
-        req.body.submission.algorithmInput,
-        req.body.submission.testFile
-        )
-        .then(([
-            algorithmTestTempDirectory,
-            cleanupCB
-        ]) => {
-
-            exec(`npm run test-javascript-algorithm-input ./server/algorithm_input_test${algorithmTestTempDirectory.slice(algorithmTestTempDirectory.lastIndexOf('/'))}/algorithm-test.js`,
-                { timeout: 5000 },
-                (err, stdout, stderr) => {
-
-                    try {
-
-                        const {
-                        testCasesStr,
-                            revisedStdoutStr
-                    } = getTestCaseOutcomes(stdout)
-
-
-                        const testCasesArr = JSON.parse(testCasesStr.trim())
-
-                        if (err) {
-                            cleanupCB()
-                            console.error('EXECUTION ERROR______________________', err)
-                        }
-
-                        cleanupCB()
-
-                        let results;
-
-                        if (
-                            testCasesArr
-                            && !testCasesArr.find(testCase => testCase.outcome === 'failed')
-                        ) {
-                            results = {
-                                testCasesArr,
-                                rawOutput: '\n' + stderr + '\n' + revisedStdoutStr,
-                                allPassed: true
-                            }
-                        } else {
-                            results = {
-                                testCasesArr,
-                                rawOutput: '\n' + stderr + '\n' + revisedStdoutStr,
-                                allPassed: false
-                            }
-                        }
-
-                        res.send(results)
-
-                    } catch (err1) {
-                        cleanupCB()
-                        console.error('CAUGHT ERROR____________________', err1)
-                        next(err1)
-                    }
-                })
-
-        }).catch(error => { console.error(error) })
+            res.send(results)
+          } catch (err1) {
+            cleanupCB()
+            console.error('CAUGHT ERROR____________________', err1)
+            next(err1)
+          }
+        }
+      )
+    })
+    .catch(error => {
+      console.error(error)
+    })
 })
