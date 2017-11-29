f7bcdea388919c12d9cd09cdb589cff6ac639e7b
NicholasGalarza <nicogala93@gmail.com>
2017-11-29 15:07:23 -0500

    setup form to edit a topic in the forum

---
+++ client/components/forum/editTopic.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { editTopic } from '../../store'
+
+class EditTopic extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      title: '',
+      newComment: '',
+      editToggle: '',
+      editForum: true
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="comments-top-box">
+          <h3>Edit Forum Topic</h3>
+        </div>
+        <div className="question-chat">
+          <div className="post-chat-container">
+            <form onSubmit={this.onSubmit}>
+              <span>
+                Topic:{'   '}
+                <input
+                  name="title"
+                  size="50"
+                  required="required"
+                  value={this.state.title}
+                  onChange={this.onChange}
+                />
+              </span>
+
+              <p>Edit Comments:</p>
+              <textarea
+                rows="3"
+                name="comment"
+                required="required"
+                value={this.state.newComment}
+                onChange={this.onChangeComment}
+              />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+          <div className="posted-chat-container">
+            <div className="posted-comments" />
+          </div>
+        </div>
+        <div className="comments-top-box">
+          <button onClick={this.toggleView}>Main Forum Page</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+const mapState = state => {
+  return {
+    forum: state.forum,
+    user: state.user
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    editTopic: topic => dispatch(editTopic(topic))
+  }
+}
+
+export default connect(mapState, mapDispatch)(EditTopic)
